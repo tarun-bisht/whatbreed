47887dc651c6e147bc45101ba8db32b12b6f416d
tarun-bisht <tarunbishttarun.11@gmail.com>
2019-12-26 18:21:27 +0530

    Procfile and requirements.txt

--- whatbreed/__init__.py
+++ whatbreed/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_compress import Compress
 import os
 app=Flask(__name__)
-app.config["SECRET_KEY"]=os.getenv('2a791f2c550436056dce7322650c3025479d84721e16fa250af7c3a6cfee11fb')
+app.config["SECRET_KEY"]=os.getenv('SECRET_KEY')
 compress=Compress(app)
 from whatbreed import routes
